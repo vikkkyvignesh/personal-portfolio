b3624e954f1bdae99788ea171774fa9657f74623
vikkkyvignesh <tcarts2020to2023@gmail.com>
2025-06-11 22:58:09 +0530

    2

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -33,7 +33,8 @@ const AboutMe = () => {
                 Get In Touch
               </a>
               <a
-                href=""
+                href="/public/Resume.pdf"
+                download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors  duration-300"
               >
                 Download CV
